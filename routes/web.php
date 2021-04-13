10407116f213d75f6c43dde0b5f087db6d6bf827
naily26 <nailymasruri@gmail.com>
2021-04-13 16:26:31 +0700

    Praktik - Membuat route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Use App\Http\Controller\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('articles', ArticleController::class);
