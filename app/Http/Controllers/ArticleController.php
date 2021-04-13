73437a87544542691c33c358fd01529b156ddf6f
naily26 <nailymasruri@gmail.com>
2021-04-13 17:30:45 +0700

    Praktik3 - Install DOMPDF pada project Laravel

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -85,7 +85,7 @@ class ArticleController extends Controller
         $article->content = $request->content;
 
         if ($article->featured_image && file_exists(storage_path('app/public/' . $article->featured_image))){
-            \storage::delete('public/' . $article->featured_image);
+            \Storage::delete('public/' . $article->featured_image);
         }
         $image_name = $request->file('image')->store('images','public');
         $article->featured_image = $image_name;
